fe220df1d0026a312d8b0eef9a0732d8ffd7fcb4
WonderCode22 <phoenix22@tutanota.com>
2021-02-09 00:27:49 +0800

    Update url of main views

--- scrappy/urls.py
+++ scrappy/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('arrival', include('arrival.urls')),
-    path('payout', include('payout.urls')),
-    path('office', include('office.urls')),
+    path('arrival/', include('arrival.urls')),
+    path('payout/', include('payout.urls')),
+    path('office/', include('office.urls')),
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
 ]
