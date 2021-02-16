82fb2387fc1f82597c05c619fd2426b9658eb319
WonderCode22 <phoenix22@tutanota.com>
2021-02-17 03:32:06 +0800

    send logged in user detail in office page

--- office/views.py
+++ office/views.py
@@ -141,10 +141,26 @@ class OfficeView(LoginRequiredMixin, View):
     template = 'office.html'
 
     def get(self, request):
+        rights = list(Rights.objects.filter(user=request.user).values_list("right", flat=True))
+        user_detail = {
+            "office": False,
+            "payout": False,
+            "arrival": False
+        }
 
-
-
-
+        if 'Office' in rights:
+            user_detail['office'] = True
+        if 'Payout' in rights:
+            user_detail['payout'] = True
+        if 'Arrival' in rights:
+            user_detail['arrival'] = True
+        user = ScrappyUser.objects.filter(id=request.user.id).first()
+        user_serialized = UserSerializer(user)
+        user_detail.update(user_serialized.data)
+        context = {
+            "user": json.dumps(user_detail)
+        }
+        print(context)
         return render(request, self.template, context)
 
 
