5c9d6bf3e846c928a291f2f7a72ad9cfc57d77d6
WonderCode22 <phoenix22@tutanota.com>
2021-05-06 13:12:30 +0200

    update mail port for MailDev

--- scrappy/settings.py
+++ scrappy/settings.py
@@ -159,6 +159,6 @@ INVITATIONS_INVITATION_MODEL = 'office.CustomInvitation'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 1025
 EMAIL_HOST_USER = 'soft2dev2021@gmail.com'
 EMAIL_HOST_PASSWORD = 'eoshsury!@34'
